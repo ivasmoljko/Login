9cf5d0a7eff2d09a14bd53788d5f42d0a85d2809
ivasmoljko <ivasmoljko1909@gmail.com>
2021-04-30 11:33:51 +0200

    dodavanje predmeta

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 
 function App() {
+  const [todos, setTodo] = React.useState([])
+  function addTodo(todo){
+    setTodo([todo, ...todos])
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React Todo</p>
+        <TodoForm addTodo= {addTodo} />
+        <TodoList todos = {todos} />
       </header>
     </div>
   );
---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo({ todo }) {
+    return ( 
+        <div style={{display:"flex"}} > 
+        <input type = "checkbox" />
+        <li style={{ color: "white", textDecoration: todo.completed? "Line-trough": null}}
+        >{todo.task}</li>
+        <button>X</button>
+        </div>
+    )
+}
+
+export default Todo
